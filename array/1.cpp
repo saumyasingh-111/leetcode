2acd8dd8ed704f6c0c57b5e89f86f799e2407393
saumya singh <saumyasingh3234@gmail.com>
2025-12-30 17:14:23 +0530

    2sum

---
+++ array/1.cpp
@@ -0,0 +1,24 @@
+// 1. 2sum
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<int> arr = {1, 2, 3, 4, 5};
+int n = arr.size();
+
+for(int i = 0; i < n; i++) {
+    for(int j = i + 1; j < n; j++) {
+        for(int k = j + 1; k < n; k++) {
+            if
+        }
+    }
+}
+
+    }
+}
+}
